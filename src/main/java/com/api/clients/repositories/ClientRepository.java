1b7568f1e7b8674a300d0e18ce50538b3b304278
Sergey Kravets <spring.javaa@yahoo.com>
2023-09-04 19:59:06 +0300

    add repos2

--- src/main/java/com/api/clients/repositories/ClientRepository.java
+++ src/main/java/com/api/clients/repositories/ClientRepository.java
@@ -1,4 +1,9 @@
 package com.api.clients.repositories;
 
-public interface ClientRepository {
+import com.api.clients.models.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Integer> {
 }
